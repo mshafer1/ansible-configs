defae0981e7569199ee07d9bafc973c663d61853
mshafer1 <matthew@ansible-host>
2023-04-01 16:58:31 -0500

    add handling path

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -9,3 +9,11 @@ apt-get install python3 python3-pip
 
 # Use pip to install ansible
 pip3 install --user ansible
+
+# If pip binary path is not on the PATH var, add it to profile file
+echo $PATH | grep "\.local" > /dev/null || echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
+
+
+# If pip binary path is not on the PATH var, add it PATH for this session
+echo $PATH | grep "\.local" > /dev/null || export PATH=$PATH:~/.local/bin
+
