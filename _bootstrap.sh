8bf733c70cb770b0becf8c8a680a754f5d507b3e
mshafer1 <matthew@ansible-host>
2023-04-01 16:26:57 -0500

    add comments

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -1,4 +1,11 @@
+# bail if we hit an error
 set -e
+
+# update indexes
 apt-get update
+
+# install Python3 and pip3
 apt-get install python3 python3-pip
+
+# Use pip to install ansible
 pip3 install --user ansible
