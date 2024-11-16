dc3a7d614a8491c0c340bd18fd40e4c77035551b
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-11-16 10:07:11 -0600

    add support for check specific title and message template

--- playbooks/roles/sensu_backend/files/sensu_scripts/ntfy-send.sh
+++ playbooks/roles/sensu_backend/files/sensu_scripts/ntfy-send.sh
@@ -15,9 +15,16 @@ check_name=$(echo "${event_info}" | jq -r '.check.metadata.name')
 check_command=$(echo "${event_info}" | jq -r '.check.command')
 check_state=$(echo "${event_info}" | jq -r '.check.state')
 check_output=$(echo "${event_info}" | jq -r '.check.output')
-
+check_ntfy_template=$(echo "${event_info}" | jq -r '.check.metadata.annotations."ntfy/template" // "default"')
+check_ntfy_title=$(echo "${event_info}" | jq -r '.check.metadata.annotations."ntfy/title" // "default"')
 
 tmp_file=$(echo -n "/tmp/ntfy-message-"; uuidgen)
+
+if [ "${check_ntfy_template}" = "check_output" ]; then
+cat > "$tmp_file" << EOF
+${check_output}
+EOF
+else # assume default if not handled
 cat > "$tmp_file" << EOF
 ${check_name} for ${resource_name} is $check_state
 
@@ -26,13 +33,20 @@ ${check_name} for ${resource_name} is $check_state
     ${check_output}
 
 EOF
+fi
+
+if [ "${check_ntfy_title}" != "default" ]; then
+check_ntfy_title_message=$(eval echo "${check_ntfy_title}")
+else
+check_ntfy_title_message="${check_name} for ${resource_name} is ${check_state}"
+fi
 
 original_args="$@"
 
 IFS=, set -- "${channels}"
 for c in $@; do
 
-curl -H "Title: ${check_name} for ${resource_name} is $check_state" \
+curl -H "Title: ${check_ntfy_title_message}" \
     -H "Authorization: Bearer ${NTFY_TOKEN}" \
     --data-binary "@${tmp_file}" \
     "${NTFY_SERVER}/${c}" >> /var/log/ntfy_notifications
