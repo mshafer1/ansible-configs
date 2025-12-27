4b171145cce2c3aca2cae2b4598f18f6acd1d971
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2025-12-27 15:22:01 -0600

    setup fail over multistreamer role

---
+++ playbooks/roles/multistream_fail_over/templates/multistream-fail-over.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -euo pipefail
+trap 'echo "Error occurred on line $LINENO"; exit 1' ERR
+
+mkdir -p /var/tmp/multistreamer
+chmod 777 /var/tmp/multistreamer
+pushd /var/tmp/multistreamer
+
+clear_routes() {
+    # cleanup any from last time
+    routes=$(ip route | grep "scope link" | grep -v -e " src " -e "/" | grep -v -E -e "^10\." -e "^172\.1[6-9]\." -e "^172\.2\d\." -e "^172\.3[01]\." -e "^192.168."; exit 0)
+    if [[ -z "${routes}" ]]; then
+    echo "No old routes identified"
+    return
+    fi
+    echo "$routes" | sed -e 's/ $//g' | xargs -I{} bash -c "ip route delete {}"
+}
+
+
+#----------------------------------------------------------
+# clear routes from any previous runs
+#----------------------------------------------------------
+clear_routes
+
+#----------------------------------------------------------
+# get IPs for pushes and make set routes
+#----------------------------------------------------------
+ip_links=$(ip --json link show | jq -r '.[] | .ifname')
+{% for push in _multistream_fail_over__pushes %}
+ip_addresses=$(nslookup `echo "{{ push.url }}" \
+    | sed -r -e 's;^.*://;;' -e 's;/.*$;;g'` \
+    | sed -n -e '/answer:/,$p' \
+    | grep Address \
+    | cut -d' ' -f2
+)
+
+if [[ -n '{{ push.interface_grep | default("") }}' ]]; then
+    interface=$(echo ${ip_links} | tr ' ' '\n' | grep -E '{{ push.interface_grep | default(".*") }}')
+    echo "${ip_addresses}" | tr ' ' '\n' | xargs -I {} bash -c "ip route add {} dev ${interface}"
+fi
+{% endfor %}
+
+
+#----------------------------------------------------------
+# main loop, stream to first URL. 
+#  If it fails, stream to second, then monitor for first to come back.
+#  When/if first comes back for min_stable_time, kill second
+#----------------------------------------------------------
+{% set alphabet = "abcdefghijklmnopqrstuvwxyz" %}
+{% for push in _multistream_fail_over__pushes %}
+prog_{{ alphabet[loop.index0] }}='ffmpeg -i rtmp://localhost/live -c:a copy -c:v libx264 -preset veryfast -tune zerolatency -g 60 -keyint_min 60 -sc_threshold 0 -vf scale={{ push.scale }} -f flv -probesize 32 -analyzeduration 0 {{ push.url }}'
+{% endfor %}
+
+cleanup() {
+    echo "Shutting down..."
+    kill $(jobs -p) 2>/dev/null
+    exit
+}
+trap cleanup SIGINT SIGTERM
+
+A_PID=""
+B_PID=""
+
+while true; do
+    if [[ -z "$A_PID" ]] || ( ! kill -0 $A_PID 2>/dev/null); then
+      echo "Starting A..."
+      $prog_a 2> /dev/null >&1 &
+      A_PID=$!
+      echo "A PID: $A_PID"
+      A_Started=$SECONDS
+      sleep 1
+    fi
+
+    DURATION=$(( SECONDS - A_Started ))
+    if (kill -0 $A_PID 2>/dev/null); then # A is alive
+      # shut down B  if it's still going
+      if [[ "$DURATION" -ge 10 ]] && [[ -n "$B_PID" ]] &&  ! (kill -0 $B_PID 2>/dev/null); then
+        echo "A is stable. Stopping B..."
+        kill -9 "$B_PID" 2>/dev/null
+        B_PID=""
+      fi
+    else
+      if [[ -z "$B_PID" ]] || ( ! kill -0 $B_PID 2>/dev/null); then
+        echo "Starting B..."
+        $prog_b > /dev/null 2>&1 &
+        B_PID=$!
+        echo "B PID: $B_PID"
+      fi
+    fi
+
+    # Small delay before trying to restart A
+    sleep 1
+done
