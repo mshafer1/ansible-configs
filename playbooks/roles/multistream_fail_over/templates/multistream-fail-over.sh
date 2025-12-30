1413a7c0e9decc6ab7658bdee0c6db093e6cf467
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2025-12-30 09:13:37 -0600

    working on it

--- playbooks/roles/multistream_fail_over/templates/multistream-fail-over.sh
+++ playbooks/roles/multistream_fail_over/templates/multistream-fail-over.sh
@@ -3,61 +3,8 @@ set -euo pipefail
 trap 'echo "Error occurred on line $LINENO"; exit 1' ERR
 
 mkdir -p /var/tmp/multistreamer
-chmod 777 /var/tmp/multistreamer
+chmod 777 /var/tmp/multistreamer || true
 pushd /var/tmp/multistreamer
-
-clear_routes() {
-    # cleanup any from last time
-    routes=$(ip route | grep "scope link" | grep -v -e " src " -e "/" | grep -v -E -e "^10\." -e "^172\.1[6-9]\." -e "^172\.2\d\." -e "^172\.3[01]\." -e "^192.168."; exit 0)
-    if [[ -z "${routes}" ]]; then
-      echo "No old routes identified"
-      return
-    fi
-
-    echo "$routes" | sed -e 's/ link linkdown/ link/g' -e 's/ $//g' | xargs -I{} bash -c "ip route delete {}"
-}
-
-
-#----------------------------------------------------------
-# clear routes from any previous runs
-#----------------------------------------------------------
-clear_routes
-
-#----------------------------------------------------------
-# get IPs for pushes and set routes
-#----------------------------------------------------------
-update_routes() {
-ip_links=$(ip --json link show | jq -r '.[] | .ifname')
-# TODO: this only works if devices are already connected..., i.e., needs to run on slow schedule
-{% for push in _multistream_fail_over__pushes %}
-ip_addresses=$(nslookup `echo "{{ push.url }}" \
-    | sed -r -e 's;^.*://;;' -e 's;/.*$;;g'` \
-    | sed -n -e '/answer:/,$p' \
-    | grep Address \
-    | cut -d' ' -f2
-)
-
-if [[ -n '{{ push.interface_grep | default("") }}' ]]; then
-    interface=$(echo ${ip_links} | tr ' ' '\n' | grep -E '{{ push.interface_grep | default(".*") }}')
-    gateway=$(ip route | grep 'default via' | grep -E '{{ push.interface_grep | default(".*") }}' | sed -E -e 's/^.* via //g' -e 's/ dev .*$//g'; exit 0)
-    if [[ -n "${gateway}" ]]; then
-      echo "${ip_addresses}" | tr ' ' '\n' | xargs -I {} bash -c "ip route add {} via ${gateway} dev ${interface} || true"
-    fi
-fi
-
-
-{% endfor %}
-}
-
-update_routes
-
-# setup interval
-# (
-#   while true; do
-#     update_routes
-#     sleep 30
-#   done
-# ) &
 
 #----------------------------------------------------------
 # main loop, stream to first URL. 
@@ -66,46 +13,96 @@ update_routes
 #----------------------------------------------------------
 {% set alphabet = "abcdefghijklmnopqrstuvwxyz" %}
 {% for push in _multistream_fail_over__pushes %}
-prog_{{ alphabet[loop.index0] }}='ffmpeg -i rtmp://localhost/live -c:a copy -c:v libx264 -preset veryfast -tune zerolatency -g 60 -keyint_min 60 -sc_threshold 0 -vf scale={{ push.scale }} -f flv -probesize 32 -analyzeduration 0 {{ push.url }}'
+
+{% set bandwidth_arg = '' %}
+{% if 'bandwidth' in push %}
+{% set bandwidth_arg = push.bandwidth %}
+{% endif %}
+
+{% set fps_arg = 30 %}
+{% if 'framerate' in push %}
+{% set fps_arg = push.framerate %}
+{% endif %}
+
+{% if 'scale' not in push %}
+{% set scale_args = 'copy' %}
+{% else %}
+{% set scale_args %}libx264 -crf {{ fps_arg }} -vf scale={{ push.scale }} -preset veryfast -tune zerolatency -g {{ fps_arg * 2 }} -keyint_min {{ fps_arg * 4 }} -sc_threshold 0 {% endset %}
+{% endif %}
+prog_{{ alphabet[loop.index0] }}='ffmpeg -i rtmp://localhost/live -c:a copy -c:v {{ scale_args }} -f flv -probesize 32 -analyzeduration 0 -fflags nobuffer -rw_timeout 50000 -progress ./progress_{{ alphabet[loop.index0] }} {{ push.url }}'
+# these don't help?? -stats_period 1 -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5
 {% endfor %}
 
 cleanup() {
     echo "Shutting down..."
     kill $(jobs -p) 2>/dev/null
+    # also clean up any rogue ffmpeg streams
+    ps -aux | grep ffmpeg | grep -v grep | sed -e 's/  */ /g' | cut -d' ' -f2 | xargs -I{} bash -c 'kill -9 {}'
     exit
 }
-trap cleanup SIGINT SIGTERM
+trap cleanup SIGINT SIGTERM TERM
+
+
+get_a_progress_time() {
+modify_timestamp=$(stat progress_a | grep Modify; exit 0);
+echo "${modify_timestamp}"
+}
+
 
 A_PID=""
 B_PID=""
+last_a_progress=""
+a_is_running=""
+DURATION=0
+a_is_old_counter=0
 
 while true; do
-    if [[ -z "$A_PID" ]] || ( ! kill -0 $A_PID 2>/dev/null); then
+    if [[ -z "$A_PID" ]]; then
       echo "Starting A..."
-      $prog_a 2> /dev/null >&1 &
+      $prog_a 2> _log_a >&1 &
       A_PID=$!
       echo "A PID: $A_PID"
       A_Started=$SECONDS
-      sleep 1
+      sleep 2
+      last_a_progress=$(get_a_progress_time)
+      a_is_old_counter=0
     fi
 
+    current_a_progress=$(get_a_progress_time)
+    echo "Current is ${current_a_progress}"
+    echo "Last    is ${last_a_progress}"
     DURATION=$(( SECONDS - A_Started ))
-    if (kill -0 $A_PID 2>/dev/null); then # A is alive
+    echo "Duration is $DURATION"
+    if [[ "${last_a_progress}" == "${current_a_progress}" ]] && [[ "$DURATION" -gt 10 ]]; then
+      a_is_old_counter=$(( a_is_old_counter + 1))
+    else
+      a_is_old_counter=0
+    fi
+    if [[ -n "${current_a_progress}" ]] && [ "${a_is_old_counter}" -gt "5" ]; then
+      echo "A is hung, killing it"
+      kill -9 "$A_PID" || true # don't die if we fail to kill
+      A_PID=""
+    fi
+    if [[ -n "$A_PID" ]] && (kill -0 $A_PID 2>/dev/null) && [[ "${a_is_old_counter}" -lt "5" ]]; then # A is alive
       # shut down B  if it's still going
-      if [[ "$DURATION" -ge 10 ]] && [[ -n "$B_PID" ]] &&  ! (kill -0 $B_PID 2>/dev/null); then
+      echo "Checking if should shut down B -> $B_PID"
+      if [[ "$DURATION" -ge 15 ]] && [[ -n "$B_PID" ]] && (kill -0 $B_PID 2>/dev/null); then
         echo "A is stable. Stopping B..."
-        kill -9 "$B_PID" 2>/dev/null
+        kill -9 "$B_PID" || true
         B_PID=""
       fi
     else
-      if [[ -z "$B_PID" ]] || ( ! kill -0 $B_PID 2>/dev/null); then
+      echo "A is dead, checking to start B"
+      if [[ -z "$B_PID" ]] || ( ! kill -0 $B_PID 2>/dev/null ); then
         echo "Starting B..."
-        $prog_b > /dev/null 2>&1 &
+        $prog_b > _log_b 2>&1 &
         B_PID=$!
         echo "B PID: $B_PID"
       fi
     fi
+    last_a_progress="${current_a_progress}"
 
     # Small delay before trying to restart A
+    echo "sleeping for a moment"
     sleep 1
 done
---
+++ playbooks/roles/multistream_fail_over/templates/multistream-ip-routes.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -euo pipefail
+trap 'echo "Error occurred on line $LINENO"; exit 1' ERR
+
+mkdir -p /var/tmp/multistreamer
+chmod 777 /var/tmp/multistreamer || true
+pushd /var/tmp/multistreamer
+
+#----------------------------------------------------------
+# get IPs for pushes and set routes
+#----------------------------------------------------------
+update_routes() {
+ip_links=$(ip --json link show | jq -r '.[] | .ifname')
+declare -a all_ips=()
+
+# clear the file
+echo '' > _route_changes
+echo '' > _new_routes
+
+old_routes=$(ip route | grep "via" | grep -v -e " src " -e "/" | grep -v -E -e "^10\." -e "^172\.1[6-9]\." -e "^172\.2\d\." -e "^172\.3[01]\." -e "^192\.168\." | sed -E -e 's/via .*$//g' -e 's/ /|/g' | tr -d '\n' | sed -e 's/|$//g'; exit 0)
+echo "old routes is: ${old_routes}"
+
+{% for push in _multistream_fail_over__pushes %}
+domain_name=$(echo "{{ push.url }}" | sed -r -e 's;^.*://;;' -e 's;/.*$;;g')
+ip_addresses=$(nslookup ${domain_name} | sed -n -e '/answer:/,$p' | grep Address | cut -d' ' -f2 | grep -v ":"; exit 0)
+
+for ip in ${ip_addresses}; do
+all_ips+=("$ip")
+done
+
+if [[ -n '{{ push.interface_grep | default("") }}' ]]; then
+    interface=$(echo ${ip_links} | tr ' ' '\n' | grep -E '{{ push.interface_grep | default(".*") }}')
+    gateway=$(ip route | grep 'default via' | grep -E '{{ push.interface_grep | default(".*") }}' | sed -E -e 's/^.* via //g' -e 's/ dev .*$//g'; exit 0)
+    if [[ -n "${gateway}" ]]; then
+      echo "Configuring to send all traffic destined for ${domain_name} out ${interface} via ${gateway}"
+      echo "-- ${ip_addresses}"
+      (echo "${ip_addresses}" | sed -e 's/^/ip route add /g' -e "s/\$/ via ${gateway} dev ${interface}/g" | tee -a _new_routes | sed -e '/^$/d' | grep -E -v "${old_routes:-/}" | tee -a _route_changes || true)
+    else
+      echo "No gateway found for ${interface}, skipping"
+    fi
+fi
+
+echo "finished with ${domain_name}"
+
+{% endfor %}
+
+# cleanup any from last time
+pattern=$(IFS="|"; echo "${all_ips[*]}")
+echo "pattern is ${pattern}"
+old_routes_to_remove=$(ip route | grep "via" | grep -v -e " src " -e "/" | grep -v -E -e "^10\." -e "^172\.1[6-9]\." -e "^172\.2\d\." -e "^172\.3[01]\." -e "^192\.168\." | grep -v -E "$pattern"; exit 0)
+
+if [[ -z "${old_routes_to_remove}" ]]; then
+    echo "No old routes identified"
+else
+    echo "Removing routes that no longer match: ${old_routes_to_remove}"
+    echo "$old_routes_to_remove" | sed -e 's/ link linkdown/ link/g' -e 's/ $//g' | sed -e 's/^/ip route delete /g' | tee -a _route_changes
+fi
+
+# echo "Would apply"
+cat _route_changes
+bash _route_changes
+}
+
+# called on schedule as service
+update_routes
