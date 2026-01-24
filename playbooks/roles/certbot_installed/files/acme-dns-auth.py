6daf676e4d6acd7bb55c3c941edfd484a9697a7b
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2026-01-23 20:37:26 -0600

    update acme-dns-auth.py

--- playbooks/roles/certbot_installed/files/acme-dns-auth.py
+++ playbooks/roles/certbot_installed/files/acme-dns-auth.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python3
 
 # acme-dns-auth.py
-# from https://github.com/joohoi/acme-dns-certbot-joohoi/blob/master/acme-dns-auth.py
-# Edits:
-# mshafer1 - change shebang to python3
+# cloned from https://github.com/mshafer1/acme-dns-certbot-joohoi/blob/master/acme-dns-auth.py
 
 import json
 import os
+import pathlib
 import requests
 import sys
+
+__license__ = """
+MIT License
+
+Copyright (c) 2018 Joona Hoikkala
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__changes__ = """
+Joona Hoikkala - initial release
+mshafer1 - change shebang to python3
+mshafer1 - add mTLS support
+mshafer1 - make backend server URL default empty to force configuring (and allow config via conf.json)
+mshafer1 - handle when response does not contain json
+"""
 
 ### EDIT THESE: Configuration values ###
 
 # URL to acme-dns instance
-ACMEDNS_URL = "https://auth.acme-dns.io"
+ACMEDNS_URL = ""  # e.g., "https://acme-dns.example.com"
 # Path for acme-dns credential storage
 STORAGE_PATH = "/etc/letsencrypt/acmedns.json"
 # Whitelist for address ranges to allow the updates from
@@ -22,8 +53,46 @@ ALLOW_FROM = []
 # Force re-registration. Overwrites the already existing acme-dns accounts.
 FORCE_REGISTER = False
 
+# Optional, mTLS region
+# NOTE: ACMDEDNS_URL must have SSL enabled (httpS://) for this to have an effect
+USE_MTLS = False # change to True to present client certificate
+MTLS_CERT_PATH = "/etc/letsencrypt/acmedns_mtls_client_cert.pem"
+MTLS_KEY_PATH = "/etc/letsencrypt/acmedns_mtls_client_key.pem"
+_SKIP_VERIFY_SERVER_CERT = False  # True to skip server cert verification (not recommended)
+SERVER_CERT_PATH = ""  # Path to custom server certificate (if not using system CA store)
+
 ###   DO NOT EDIT BELOW THIS POINT   ###
 ###         HERE BE DRAGONS          ###
+
+def _env_var_or_default(varname, conf: dict[str, str], default=""):
+    """Helper to load environment variable or return default value"""
+    conf_value = conf.get(varname, None)
+    return os.environ.get("ACME_DNS__" + varname.upper(), conf_value or default)
+
+def _load_config():
+    """Loads configuration from environment variables or neighboring conf.json file"""
+    global ACMEDNS_URL, STORAGE_PATH, ALLOW_FROM, FORCE_REGISTER, USE_MTLS, MTLS_CERT_PATH, MTLS_KEY_PATH, _SKIP_VERIFY_SERVER_CERT, SERVER_CERT_PATH
+    # Load from conf.json if it exists
+    conf_path = pathlib.Path(__file__).parent / "conf.json"
+    data = {}
+    if conf_path.is_file():
+        try:
+            with conf_path.open() as fin:
+                data = json.load(fin)
+        except (IOError, ValueError):
+            print("ERROR: conf.json file exists but could not be loaded.")
+            sys.exit(1)
+    ACMEDNS_URL = _env_var_or_default("acmedns_url", data, ACMEDNS_URL)
+    STORAGE_PATH = _env_var_or_default("storage_path", data, STORAGE_PATH)
+    ALLOW_FROM = _env_var_or_default("allow_from", data, ALLOW_FROM)
+    FORCE_REGISTER = _env_var_or_default("force_register", data, FORCE_REGISTER)
+    USE_MTLS = _env_var_or_default("use_mtls", data, USE_MTLS)
+    MTLS_CERT_PATH = _env_var_or_default("mtls_cert_path", data, MTLS_CERT_PATH)
+    MTLS_KEY_PATH = _env_var_or_default("mtls_key_path", data, MTLS_KEY_PATH)
+    if (USE_MTLS or MTLS_CERT_PATH or MTLS_KEY_PATH) and (not os.path.exists(MTLS_CERT_PATH) or not os.path.exists(MTLS_KEY_PATH)):
+        print("ERROR: mTLS is enabled but client certificate or key file does not exist.")
+        sys.exit(1)
+    SERVER_CERT_PATH = _env_var_or_default("server_cert_path", data, SERVER_CERT_PATH)
 
 DOMAIN = os.environ["CERTBOT_DOMAIN"]
 if DOMAIN.startswith("*."):
@@ -43,13 +112,21 @@ class AcmeDnsClient(object):
     def register_account(self, allowfrom):
         """Registers a new ACME-DNS account"""
 
+        mtls_args = {}
+        if USE_MTLS:
+            mtls_args['cert'] = (MTLS_CERT_PATH, MTLS_KEY_PATH)
+
         if allowfrom:
             # Include whitelisted networks to the registration call
             reg_data = {"allowfrom": allowfrom}
             res = requests.post(self.acmedns_url+"/register",
-                                data=json.dumps(reg_data))
+                                data=json.dumps(reg_data),
+                                verify=SERVER_CERT_PATH or (not _SKIP_VERIFY_SERVER_CERT),
+                                **mtls_args)
         else:
-            res = requests.post(self.acmedns_url+"/register")
+            res = requests.post(self.acmedns_url+"/register", 
+                                verify=SERVER_CERT_PATH or (not _SKIP_VERIFY_SERVER_CERT),
+                                **mtls_args)
         if res.status_code == 201:
             # The request was successful
             return res.json()
@@ -66,9 +143,14 @@ class AcmeDnsClient(object):
         headers = {"X-Api-User": account['username'],
                    "X-Api-Key": account['password'],
                    "Content-Type": "application/json"}
+        mtls_args = {}
+        if USE_MTLS:
+            mtls_args['cert'] = (MTLS_CERT_PATH, MTLS_KEY_PATH)
         res = requests.post(self.acmedns_url+"/update",
                             headers=headers,
-                            data=json.dumps(update))
+                            data=json.dumps(update),
+                            verify=SERVER_CERT_PATH or (not _SKIP_VERIFY_SERVER_CERT),
+                            **mtls_args)
         if res.status_code == 200:
             # Successful update
             return
@@ -81,7 +163,10 @@ class AcmeDnsClient(object):
                    "------- Response body: {}")
             s_headers = json.dumps(headers, indent=2, sort_keys=True)
             s_update = json.dumps(update, indent=2, sort_keys=True)
-            s_body = json.dumps(res.json(), indent=2, sort_keys=True)
+            try:
+                s_body = json.dumps(res.json(), indent=2, sort_keys=True)
+            except ValueError:
+                s_body = res.text
             print(msg.format(s_headers, s_update, res.status_code, s_body))
             sys.exit(1)
 
@@ -140,6 +225,7 @@ class Storage(object):
 
 if __name__ == "__main__":
     # Init
+    _load_config()
     client = AcmeDnsClient(ACMEDNS_URL)
     storage = Storage(STORAGE_PATH)
 
